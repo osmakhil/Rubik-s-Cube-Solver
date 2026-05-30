bef75fa7fb823d039cce7919ce58a239154fa63a
Akhil <osmakhil@Akhils-MacBook-Air.local>
2026-05-30 18:46:09 +0530

    Initial commit

---
+++ Model/RubikCube.cpp
@@ -0,0 +1,116 @@
+#include "RubikCube.hpp"
+
+//
+// Given a color return it's first letter
+//
+
+char RubiksCube::getColorLetter(COLOR color)
+{
+    switch (color)
+    {
+    case COLOR::BLUE:
+        return 'B';
+    case COLOR::GREEN:
+        return 'G';
+    case COLOR::RED:
+        return 'R';
+    case COLOR::YELLOW:
+        return 'Y';
+    case COLOR::WHITE:
+        return 'W';
+    case COLOR::ORANGE:
+        return 'O';
+    }
+}
+
+/*
+ * Describe a move using an index
+ */
+string RubiksCube::getMove(MOVE ind)
+{
+    switch (ind)
+    {
+    case MOVE::L:
+        return "L";
+    case MOVE::LPRIME:
+        return "L'";
+    case MOVE::L2:
+        return "L2";
+    case MOVE::R:
+        return "R";
+    case MOVE::RPRIME:
+        return "R'";
+    case MOVE::R2:
+        return "R2";
+    case MOVE::U:
+        return "U";
+    case MOVE::UPRIME:
+        return "U'";
+    case MOVE::U2:
+        return "U2";
+    case MOVE::D:
+        return "D";
+    case MOVE::DPRIME:
+        return "D'";
+    case MOVE::D2:
+        return "D2";
+    case MOVE::F:
+        return "F";
+    case MOVE::FPRIME:
+        return "F'";
+    case MOVE::F2:
+        return "F2";
+    case MOVE::B:
+        return "B";
+    case MOVE::BPRIME:
+        return "B'";
+    case MOVE::B2:
+        return "B2";
+    }
+}
+
+/*
+ * Perform a move operation on using a Move index.
+ */
+RubiksCube &RubiksCube::move(MOVE ind)
+{
+    switch (ind)
+    {
+    case MOVE::L:
+        return this->l();
+    case MOVE::LPRIME:
+        return this->lPrime();
+    case MOVE::L2:
+        return this->l2();
+    case MOVE::R:
+        return this->r();
+    case MOVE::RPRIME:
+        return this->rPrime();
+    case MOVE::R2:
+        return this->r2();
+    case MOVE::U:
+        return this->u();
+    case MOVE::UPRIME:
+        return this->uPrime();
+    case MOVE::U2:
+        return this->u2();
+    case MOVE::D:
+        return this->d();
+    case MOVE::DPRIME:
+        return this->dPrime();
+    case MOVE::D2:
+        return this->d2();
+    case MOVE::F:
+        return this->f();
+    case MOVE::FPRIME:
+        return this->fPrime();
+    case MOVE::F2:
+        return this->f2();
+    case MOVE::B:
+        return this->b();
+    case MOVE::BPRIME:
+        return this->bPrime();
+    case MOVE::B2:
+        return this->b2();
+    }
+}
